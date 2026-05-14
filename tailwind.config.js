a172bb0355587225ac506c6708dd457ee0fa6a97
Haaams <ilhamprazsetyo33@gmail.com>
2026-05-14 13:54:27 +0700

    fix: add layout for Tailwind import, fix productName, add test script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{svelte,ts,html}', './*.html'],
+  content: ['./src/**/*.{svelte,ts,html}'],
   theme: { extend: {} },
   plugins: [],
 }
